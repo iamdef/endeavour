96194af258491ee39cf28a72cc8c95f92d6e33cf
Dmitry <xolodetzzz@yandex.ru>
2022-06-04 22:26:32 +0300

    Created a gallery with rotating match cards: by click for mobile devices and by mouse hover for desktop

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// waves
+
 VANTA.WAVES({
     el: "header",
     mouseControls: true,
@@ -10,8 +12,39 @@ VANTA.WAVES({
     color: 0x70706
   })
 
+// menu
+
 $('.menu-btn').on('click', function(e) {
     e.preventDefault();
     $(this).toggleClass('menu-btn_active');
     $('.menu').toggleClass('menu_active');
 });
+
+// rotate cards
+
+var $width = $(window).width()
+
+var flipCards = ['#firstCard', '#secondCard', '#thirdCard', '#fourthCard'];
+
+
+for (let i = 0; i < flipCards.length; i++) {
+
+    let elementFront = flipCards[i] + ' .shedule-logo-wrapper';
+    let elementBack = flipCards[i] + ' .back-shedule';
+
+    if ($width < 480) {
+        $(flipCards[i]).on('click', function() {
+            $(elementFront).toggleClass('shedule-logo-wrapper_active');
+            $(elementBack).toggleClass('back-shedule_active');
+        });
+    } else {
+        $(flipCards[i]).hover(function() {
+            $(elementFront).toggleClass('shedule-logo-wrapper_active');
+            $(elementBack).toggleClass('back-shedule_active');
+        });
+    }
+}
+
+
+
+
