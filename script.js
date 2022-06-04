1be88ec7b835737986b5237f8f293d327e0b8755
Dmitry <xolodetzzz@yandex.ru>
2022-06-04 16:26:18 +0300

    Created the layout of the start page, linked with styles, added a menu button

---
+++ script.js
@@ -0,0 +1,17 @@
+VANTA.WAVES({
+    el: "header",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    scale: 1.00,
+    scaleMobile: 1.00,
+    color: 0x70706
+  })
+
+$('.menu-btn').on('click', function(e) {
+    e.preventDefault();
+    $(this).toggleClass('menu-btn_active');
+    $('.menu').toggleClass('menu_active');
+});
