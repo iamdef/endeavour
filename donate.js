06400e50fff3ed4781eaae9e82142c3bf24335a2
Dmitry <xolodetzzz@yandex.ru>
2022-06-18 15:36:55 +0300

    Rewritten adaptive styles for donate.html

--- donate.js
+++ donate.js
@@ -17,3 +17,19 @@ $('.menu-btn').on('click', function(e) {
     $(this).toggleClass('menu-btn_active');
     $('.menu').toggleClass('menu_active');
 });
+
+function copyToClipboard(el) {
+    var $tmp = $("<input>");
+    $("body").append($tmp);
+    $tmp.val($(el).text()).select();
+    document.execCommand("copy");
+    $tmp.remove();
+}
+
+$("#btc-btn").on("click", function() {
+  copyToClipboard("#btc");
+});
+
+$("#usdt-btn").on("click", function() {
+  copyToClipboard("#usdt");
+});
