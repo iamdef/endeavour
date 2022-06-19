32a2b9f2df7a0f1f68374c202d34908aa1f6416e
Dmitry <xolodetzzz@yandex.ru>
2022-06-19 16:22:50 +0300

    Now charts are created by click, and not hidden before it.

--- team.js
+++ team.js
@@ -36,26 +36,42 @@ $(document).ready(function(){
     });
   });
 
-  (function($) {
-    $(function() {
+//   (function($) {
+//     $(function() {
       
-      $('ul.tabs__caption').on('click', 'li:not(.active)', function() {
-        $(this)
-          .addClass('active').siblings().removeClass('active')
-          .closest('div.tabs').find('div.tabs__content').removeClass('active').eq($(this).index()).addClass('active');
-      });
+//       $('ul.tabs__caption').on('click', 'li:not(.active)', function() {
+//         $(this)
+//           .addClass('active').siblings().removeClass('active')
+//           .closest('div.tabs').find('div.tabs__content').removeClass('active').eq($(this).index()).addClass('active');
+//       });
       
-    });
-    })(jQuery);
+//     });
+//     })(jQuery);
 
 /* <canvas id="myChart" width="400" height="400"></canvas> */
+let players = ['xolodetzzz', 'THE_Doctor_Art', 'HarutAdyan', 'R_Kanj228', 'DavSum', 'ND102030', 'MasterJediko', 'Adzel', 'Rabchevsky', 'Selya', 'just_alyosha', 'DDDDENNYYYY'];
 const matches = document.getElementById('matches');
-const strikes = document.getElementById('strikers');
+const strikers = document.getElementById('strikers');
 const assists = document.getElementById('assists');
 const otbor = document.getElementById('otbor');
 const perexvat = document.getElementById('perexvat');
 const passes = document.getElementById('passes');
 let aspectRatio;
+let canvas = document.querySelector('.chart-wrapper');
+let domMatches = document.getElementById('matсhes-li');
+let domGoals = document.getElementById('goals-li');
+let domAssists = document.getElementById('assists-li');
+let domOtbor = document.getElementById('otbor-li');
+let domPerexvat = document.getElementById('perexvat-li');
+let domPasses = document.getElementById('passes-li');
+
+domMatches.addEventListener('click', () => createGraph("matches", "Количество матчей"));
+domGoals.addEventListener('click', () => createGraph("strikers", "Бомбардиры"));
+domAssists.addEventListener('click', () => createGraph("assists", "Голевые передачи"));
+domOtbor.addEventListener('click', () => createGraph("otbor", "Отборы"));
+domPerexvat.addEventListener('click', () => createGraph("perexvat", "Перехваты"));
+domPasses.addEventListener('click', () => createGraph("passes", "Точность передач"));
+
 
 if ($width > 768) {
     aspectRatio = 2
@@ -109,7 +125,7 @@ const MATCHES = new Chart(matches, {
     }
 });
 
-const STRIKES = new Chart(strikes, {
+const STRIKES = new Chart(strikers, {
     type: 'bar',
     data: {
         labels: ['xolodetzzz', 'THE_Doctor_Art', 'HarutAdyan', 'R_Kanj228', 'DavSum', 'ND102030', 'MasterJediko', 'Adzel', 'Rabchevsky', 'Selya', 'just_alyosha', 'DDDDENNYYYY'],
@@ -338,3 +354,55 @@ const PASSES = new Chart(passes, {
         }
     }
 });
+
+function createGraph(id, label) {
+    canvas.innerHTML = `<div class="bar-wrapper"><canvas class="bar" id=${id}></canvas></div>`;
+    const el = document.getElementById(id);
+    const GRAPH = new Chart(el, {
+        type: 'bar',
+        data: {
+            labels: players,
+            datasets: [{
+                label: label,
+                data: [2, 4, 3, 5, 2, 3, 6, 3, 12, 5, 7, 8],
+                backgroundColor: [
+                    'rgba(255, 159, 64, 0.2)',
+                    'rgba(255, 159, 64, 0.2)',
+                    'rgba(255, 159, 64, 0.2)',
+                    'rgba(255, 159, 64, 0.2)',
+                    'rgba(255, 159, 64, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255, 159, 64, 1)',
+                    'rgba(255, 159, 64, 1)',
+                    'rgba(255, 159, 64, 1)',
+                    'rgba(255, 159, 64, 1)',
+                    'rgba(255, 159, 64, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            aspectRatio: aspectRatio,
+            indexAxis: 'y',
+            scales: {
+                y: {
+                    beginAtZero: true,
+                    ticks: {
+                        stepSize: 1
+                    }
+                },
+                x: {
+                    beginAtZero: true,
+                    ticks: {
+                        stepSize: 1
+                    }
+                }
+            }
+        }
+    });    
+}
+
+
